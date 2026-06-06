3355390b0980fce8614df6d1cbf74a5204754e4b
alexbelov12 <sanektwixcr@gmail.com>
2026-06-06 06:24:43 +0500

    Switch to pg8000 driver

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ class Config:
 
     _db_url = os.environ.get('DATABASE_URL', 'sqlite:///platform.db')
     if _db_url.startswith('postgres://'):
-        _db_url = _db_url.replace('postgres://', 'postgresql://', 1)
+        _db_url = _db_url.replace('postgres://', 'postgresql+pg8000://', 1)
+    elif _db_url.startswith('postgresql://'):
+        _db_url = _db_url.replace('postgresql://', 'postgresql+pg8000://', 1)
     SQLALCHEMY_DATABASE_URI = _db_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
